21257091414db89db730275ead64723608b7a262
Bryan <bryan@asiasigma.com>
2024-08-23 02:03:29 +0800

    add single animation, quote, and picture

--- pages/invitation/[...slug].js
+++ pages/invitation/[...slug].js
@@ -25,6 +25,13 @@ export default function InvitationPage(props) {
       setSong(music);
       music.play();
       music.loop = true;
+      
+      setTimeout(() => {
+         const mainDiv = document.getElementById('main');
+         if (mainDiv) {
+            mainDiv.scrollIntoView({ behavior: 'smooth' });
+         }
+      }, 100);
    }
 
    function stopMusic() {
@@ -119,13 +126,24 @@ export default function InvitationPage(props) {
          
        },
        {
-         src: '/Gallery/2.jpeg',
+         src: '/Gallery/11.jpeg',
          thumbnailWidth: 1000,  // Larger width
          thumbnailHeight: 800, // Larger height
          
        },
        {
-         src: '/Gallery/3.jpeg',
+         src: '/Gallery/12.jpeg',
+         thumbnailWidth: 1000,  // Larger width
+         thumbnailHeight: 800, // Larger height
+         
+       },
+       {
+         src: '/Gallery/14.jpeg',
+         thumbnailWidth: 1000,  // Larger width
+         thumbnailHeight: 800, // Larger height
+         
+       },       {
+         src: '/Gallery/13.jpeg',
          thumbnailWidth: 1000,  // Larger width
          thumbnailHeight: 800, // Larger height
          
@@ -169,37 +187,153 @@ export default function InvitationPage(props) {
             <title>Inviation {nama}</title>
             <meta name="description" />
          </Head>
-         <button className="fixed bottom-3 start-3" onClick={() => stopMusic()}>
-            Stop
-         </button>
-         <div className="grid grid-cols-1 grid-rows-1 justify-items-center h-sceen align-middle">
-            <div className="">
-               {/* <Image src={""} /> */}
-               <p className="font-bold ">Kepada {nama}</p>
-               <p>Meja {meja}</p>
+         {show && (
+            <button className="fixed bottom-3 start-3" onClick={() => stopMusic()}>
+               Stop
+            </button>
+         )}
+         <div
+            className="flex flex-col justify-end items-center h-screen bg-cover bg-center relative overflow-hidden"
+            style={{
+               backgroundImage: "url('/Gallery/6.jpeg')",
+            }}>
+            <div
+               className="absolute inset-0 bg-gradient-to-b from-transparent to-white"
+               aria-hidden="true"></div>
+            <div className="text-center bg-white bg-opacity-50 p-8 rounded-3xl mb-8 relative z-10">
+               <p className="md:text-3xl">Kepada Bapak/Ibu/Saudara (i)</p>
+               <p className="font-semibold md:text-3xl md:my-2">{nama}</p>
+               <p className="md:text-3xl">Meja {meja}</p>
                <button
-                  className="my-2 py-2 px-3 bg-red-400 rounded-lg hover:bg-red-500 text-white"
+                  className="md:text-xl my-2 py-2 px-3 bg-red-400 md:mt-4 rounded-lg hover:bg-red-500 text-white"
                   onClick={() => BukaUndangan()}>
                   💞 Buka Undangan 💞
                </button>
             </div>
          </div>
          {show && (
-            <div className="container mx-auto p-4">
+            <div id="main" className="container mx-auto p-4 relative overflow-hidden">
+
+               {/* Intro */}
+               <div className="container text-center my-4">
+                  <div className="flex justify-center my-6 md:my-10">
+                     <img
+                        className="w-full max-w-[200px] md:max-w-[400px] object-contain"
+                        data-aos="zoom-in"
+                        data-aos-delay="500"
+                        src="/hias.png"
+                        alt="hiasan"
+                     />
+                  </div>
+                  <h1 data-aos='fade-down' data-aos-delay="600" className="md:text-3xl italic font-semibold">
+                     "Maka hendaklah sekarang ini kelebihan kamu mencukupkan kekurangan mereka, 
+                     agar kelebihan mereka kemudian mencukupkan kekurangan kamu, 
+                     supaya ada keseimbangan."
+                  </h1>
+                  <p data-aos='fade-down' data-aos-delay="700" className="my-4 md:text-2xl italic font-bold">2 Korintus 8:14</p>
+               </div>
+
+               {/* Profile */}
+               <div className="container my-4">
+                  {/* The Groom */}
+                  <div className="container mx-auto">
+                     <div className="flex flex-col lg:flex-row lg:space-x-4">
+                        <div data-aos='fade-right' data-aos-delay="800" className="my-4 flex-1 border border-black rounded-se-[120px] rounded-bl-[120px] overflow-hidden">
+                           <img src={'/Profile/kevin1.jpeg'} className="object-cover" alt="The Groom" />
+                        </div>
+                        <div className="my-4 flex-1 lg:p-4">
+                           <button data-aos='fade-up-left' className="cursor-default border border-solid border-black rounded-3xl py-2 px-4 lg:text-3xl">The Groom</button>
+                           <p data-aos='fade-up' className="mt-4 lg:mt-6 text-4xl font-semibold lg:text-6xl">Kevin Lois</p>
+                           <p data-aos='zoom-in' className="md:text-3xl">
+                              Kevin is a cheerful, friendly, and caring person. He brings positivity and warmth wherever he goes, 
+                              making everyone around him feel welcome and valued. On this special day, 
+                              Kevin is excited to share his joy and love with the people who mean the most to him, 
+                              as he starts this new chapter in his life.
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+
+                  {/* Bridge */}
+                  <div className="flex justify-center my-6 md:my-10">
+                     <img
+                        className="w-full max-w-[200px] md:max-w-[400px] object-contain"
+                        data-aos="zoom-in"
+                        data-aos-delay="500"
+                        src="/batas.png"
+                        alt="Flower Chain Divider"
+                     />
+                  </div>
+
+                  {/* <hr  className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" /> */}
+                  
+                  {/* The Bride */}
+                  <div className="container mx-auto">
+                     <div className="flex flex-col lg:flex-row-reverse lg:space-x-4">
+                        <div data-aos='fade-left' data-aos-delay="800" className="my-4 flex-1 border border-black rounded-ss-[120px] rounded-br-[120px] overflow-hidden">
+                           <img src={'/Profile/gisella1.jpeg'} className="object-cover" alt="The Bride" />
+                        </div>
+                        <div data-aos='zoom-in' className="my-4 flex-1 lg:p-4">
+                           <button data-aos='fade-up-right' className="cursor-default border border-solid border-black rounded-3xl py-2 px-4 lg:text-3xl">The Bride</button>
+                           <p data-aos='fade-up' className="mt-4 lg:mt-6 text-4xl font-semibold lg:text-6xl">Gisela Jeanette Mayella Sie</p>
+                           <p data-aos='zoom-in' className="md:text-3xl">
+                              Gisela navigates life with a deep sense of purpose and empathy. 
+                              Though introverted and shy, Gisela is driven by a desire to connect with others on a meaningful level. 
+                              Her kind and disciplined nature often guides her, and Gisela is deeply moved by the beauty and emotions around her. 
+                              As she embarks on this new journey, Gisela looks forward to sharing these profound and heartfelt moments with those who mean the most to her.
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
 
                {/* Wedding Announcement Info */}
-               <div className="container mx-auto text-center bg-blue-200 p-4">
-                  <h1 data-aos="fade-up" className="text-2xl my-4 md:text-6xl">
-                     Save <span className="font-cookie">The</span> Date
+               <div className="text-center p-6 md:p-8 relative">
+                  {/* Top-left decoration */}
+                  <img
+                     data-aos='fade-right'
+                     src="/top-left.png"
+                     alt="Top Left Decoration"
+                     className="absolute top-0 left-0 w-[80px] md:w-[200px] lg:w-[300px] z-10"
+                  />
+                  
+                  {/* Top-right decoration */}
+                  <img
+                     data-aos='fade-left'
+                     src="/top-right.png"
+                     alt="Top Right Decoration"
+                     className="absolute top-0 right-0  w-[80px] md:w-[200px] lg:w-[300px] z-10"
+                  />
+
+                  {/* Bottom-left decoration */}
+                  <img
+                     data-aos='fade-right'
+                     data-aos-delay="600"
+                     src="/bottom-left.png"
+                     alt="Bottom Left Decoration"
+                     className="absolute bottom-0 left-0 w-[80px] md:w-[200px] lg:w-[300px] z-10"
+                  />
+
+                  {/* Bottom-right decoration */}
+                  <img
+                     data-aos='fade-left'
+                     data-aos-delay="600"
+                     src="/bottom-right.png"
+                     alt="Bottom Right Decoration"
+                     className="absolute bottom-0 right-0 w-[80px] md:w-[200px] lg:w-[300px] z-10"
+                  />
+
+                  <h1 data-aos="fade-up" data-aos-delay="200" className="text-3xl my-4 md:text-6xl">
+                     Save <div data-aos="zoom-out" data-aos-delay="500" className="inline font-cookie text-5xl md:text-8xl">The</div> Date
                   </h1>
-                  <div data-aos="fade-up" data-aos-delay="200">
-                     <h1 className="font-cookie font-medium text-4xl md:text-6xl">Jeanette Mayella Sie S.E</h1>
-                     <span className="font-cookie font-medium text-4xl md:text-6xl">&</span>
-                     <h1 className="font-cookie font-medium text-4xl md:text-6xl">Kevin Lois</h1>
+                  <div className="md:my-6">
+                     <div data-aos='fade-up' data-aos-delay="1000" className="font-cookie font-medium text-4xl md:text-6xl">Gisela Jeanette Mayella Sie</div>
+                     <div data-aos='zoom-out' data-aos-delay="1200" className="font-cookie font-medium text-4xl md:text-6xl">&</div>
+                     <h1 data-aos='fade-up' data-aos-delay="1400" className="font-cookie font-medium text-4xl md:text-6xl">Kevin Lois</h1>
                   </div>
-                  <div data-aos="fade-up" data-aos-delay="400" className="my-4 flex flex-col items-center justify-center text-center">
-                     <h1 className="mb-4 uppercase font-semibold mb-2 md:text-4xl">Are Getting Married</h1>
-                     <img src="/date-black.png" width={200} height={200} alt="Sunday, 13 October 2024"/>
+                  <div className="my-4 md:my-6 flex flex-col items-center justify-center text-center">
+                     <h1 data-aos="flip-left" data-aos-delay="1500" className="mb-4 uppercase font-semibold mb-2 md:text-4xl">Are Getting Married</h1>
+                     <img data-aos="zoom-out" data-aos-delay="1600" src="/date-black.png" className="w-[150px] md:w-[250px]" alt="Sunday, 13 October 2024"/>
                   </div>
                   <div data-aos="fade-up" data-aos-delay="500"> 
                      <h1 className="font-semibold text-2xl md:text-5xl">Holy Matrimony</h1>
@@ -215,52 +349,8 @@ export default function InvitationPage(props) {
                   </div>
                </div>
 
-
-               {/* Profile */}
-               <div className="container my-4">
-                  {/* The Groom */}
-                  <div className="container mx-auto">
-                     <div data-aos="fade-up" data-aos-delay="1000" className="flex flex-col lg:flex-row lg:space-x-4">
-                        <div className="my-4 flex-1 border border-black rounded-se-[120px] rounded-bl-[120px] overflow-hidden">
-                           <img src={'/Profile/kevin1.jpeg'} className="object-cover" alt="The Groom" />
-                        </div>
-                        <div data-aos="fade-up" data-aos-delay="500" className="my-4 flex-1 lg:p-4">
-                           <span className="border border-solid border-black rounded-3xl py-2 px-4 lg:text-3xl">The Groom</span>
-                           <p className="mt-4 lg:mt-6 text-4xl font-semibold lg:text-6xl">Kevin Lois</p>
-                           <p className="md:text-3xl">
-                              Kevin is a cheerful, friendly, and caring person. He brings positivity and warmth wherever he goes, 
-                              making everyone around him feel welcome and valued. On this special day, 
-                              Kevin is excited to share his joy and love with the people who mean the most to him, 
-                              as he starts this new chapter in his life.
-                           </p>
-                        </div>
-                     </div>
-                  </div>
-
-                  <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
-                  
-                  {/* The Bride */}
-                  <div className="container mx-auto">
-                     <div className="flex flex-col lg:flex-row-reverse lg:space-x-4">
-                        <div data-aos="fade-up" data-aos-duration="1000" className="my-4 flex-1 border border-black rounded-ss-[120px] rounded-br-[120px] overflow-hidden">
-                           <img src={'/Profile/gisella1.jpeg'} className="object-cover" alt="The Bride" />
-                        </div>
-                        <div  data-aos="fade-up" data-aos-duration="1000" className="my-4 flex-1 lg:p-4">
-                           <span className="border border-solid border-black rounded-3xl py-2 px-4 lg:text-3xl">The Bride</span>
-                           <p className="mt-4 lg:mt-6 text-4xl font-semibold lg:text-6xl">Gisella Jeanette Mayella Sie</p>
-                           <p className="md:text-3xl">
-                              Gisela navigates life with a deep sense of purpose and empathy. 
-                              Though introverted and shy, Gisela is driven by a desire to connect with others on a meaningful level. 
-                              Her kind and disciplined nature often guides her, and Gisela is deeply moved by the beauty and emotions around her. 
-                              As she embarks on this new journey, Gisela looks forward to sharing these profound and heartfelt moments with those who mean the most to her.
-                           </p>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-
                {/* Countdown */}
-               <div data-aos="fade-up" data-aos-duration="1000" id="countdown" className="container mx-auto flex flex-col items-center justify-center text-center">
+               <div data-aos="fade-up" data-aos-duration="1000" id="countdown" className="my-6 md:my-10 container mx-auto flex flex-col items-center justify-center text-center">
                   <Countdown
                      date={targetDate}  // 10 seconds from now
                      renderer={renderer}
@@ -289,7 +379,7 @@ export default function InvitationPage(props) {
                            <div className="text-start">
                               <h2 className="mb-1 text-2xl font-semibold">Bank BCA</h2>
                               <p className="italic font-medium">0982309812</p>
-                              <p className="italic font-medium">Gisella Jeanette Mayella Sie</p>
+                              <p className="italic font-medium">gisela Jeanette Mayella Sie</p>
                            </div>
                            <button id="copy1" className="mt-1 font-medium hover:text-white" onClick={() => copyToClipboard('0982309812', 'copy1')}>{copy1}</button>
                         </div>
@@ -307,8 +397,49 @@ export default function InvitationPage(props) {
                </div>
 
                {/* Photo Gallery */}
-               <div data-aos="fade-up" data-aos-duration="1500" className="container my-4">
-                  <Gallery images={IMAGES} />
+               <div className="">
+                  <h1 data-aos="fade-up" data-aos-duration="1500" className="text-center text-3xl">Our Best Moment</h1>
+                  <div data-aos="fade-up" data-aos-duration="1500" className="grid-wrapper">
+                     <div className="tall">
+                        <img src="/Gallery/1.jpeg"/>
+                     </div>
+                     <div className="tall">
+                        <img src="/Gallery/3.jpeg"/>
+                     </div>
+                     <div className="big">
+                        <img src="/Gallery/5.jpeg"/>
+                     </div>
+                     <div className="tall">
+                        <img src="/Gallery/7.jpeg"/>
+                     </div>
+                     <div className="tall">
+                        <img src="/Gallery/2.jpeg"/>
+                     </div>
+                     <div className="big">
+                        <img src="/Gallery/4.jpeg"/>
+                     </div>
+                     <div className="tall">
+                        <img src="/Gallery/9.jpeg"/>
+                     </div>
+                     <div className="tall">
+                        <img src="/Gallery/10.jpeg"/>
+                     </div>
+                     <div className="tall">
+                        <img src="/Gallery/8.jpeg"/>
+                     </div>
+                     <div className="tall">
+                        <img src="/Gallery/11.jpeg"/>
+                     </div>
+                     {/* <div className="big">
+                        <img src="/Gallery/13.jpeg"/>
+                     </div> */}
+                     <div className="tall">
+                        <img src="/Gallery/12.jpeg"/>
+                     </div>
+                     <div className="tall">
+                        <img src="/Gallery/14.jpeg"/>
+                     </div>
+                  </div>
                </div>
 
                {/* Maps */}
